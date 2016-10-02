22f8c04b0aee652aa53e317a682cf5942bc184e8
Yan Hong <onlyhongyan@gmail.com>
2016-10-02 15:04:30 -0700

    add ticket js

---
+++ js/ticket.js
@@ -0,0 +1,19 @@
+var data = [
+	{
+		"name": "Avengers",
+		"date": "Sep. 2014",
+		"price": 15
+	},
+
+	{
+		"name": "Star Wars",
+		"date": "Oct. 2015",
+		"price": 10
+	},
+
+	{
+		"name": "Tinanic",
+		"date": "Mar. 1996",
+		"price": 6
+	}
+];
