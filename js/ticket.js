d1ee9d8fed4047f495280a3261ecdc6eb10146dc
Yan Hong <onlyhongyan@gmail.com>
2016-10-02 16:39:20 -0700

    purchase button function

--- js/ticket.js
+++ js/ticket.js
@@ -2,18 +2,50 @@ var data = [
 	{
 		"name": "Avengers",
 		"date": "Sep. 2014",
-		"price": 15
+		"price": 15,
+		"sold": false
 	},
 
 	{
 		"name": "Star Wars",
 		"date": "Oct. 2015",
-		"price": 10
+		"price": 10,
+		"sold": false
 	},
 
 	{
 		"name": "Tinanic",
 		"date": "Mar. 1996",
-		"price": 6
+		"price": 16,
+		"sold": false
 	}
 ];
+
+function buyTicket(i) {
+	data[i].sold = true;
+	loadData(data);
+}
+
+function loadData(d) {
+	$(".table-data").remove();
+	for (var i = 0; i < d.length; i++) {
+		if (d[i].price >= 10) {
+			if (d[i].sold) {
+				var button = "<button disabled>Sold Out</button>";
+			} else {
+				var button = "<button onClick='buyTicket(" + i + ")'>Purchase</button>";
+			}
+			
+			var tableRow = "<tr class='table-data'>" +
+					"<td>" + d[i].name + "</td>" + 
+					"<td>" + d[i].date +"</td>" + 
+					"<td>" + d[i].price + "</td>" + 
+					"<td>" + button + "</td>" +
+				"</tr>";
+
+			$('table tbody').append(tableRow);
+		}
+	}
+}
+
+loadData(data);
